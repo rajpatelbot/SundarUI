678cc3c6fa660b768225462ffcaffcfd23cc5835
Raj Patel <rajpatel158g@gmail.com>
2023-03-04 23:28:52 +0530

    Import "Crosstech.jsx" component in "Homepage.jsx"

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import Crosstech from "../Layout/Crosstech"
 import Hero from "../Layout/Hero"
 
 const Homepage = () => {
     return (
         <div>
             <Hero />
+            <Crosstech />
         </div>
     )
 }
