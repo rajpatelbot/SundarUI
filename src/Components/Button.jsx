b8c976e771c383ec65776d4006d44ce295753890
Raj Patel <rajpatel158g@gmail.com>
2023-03-04 15:33:27 +0530

    Replace "class" to "className" in "Button.jsx"

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 export const Orange = ({ text }) => {
   return (
     <a href="#!">
-      <button class="bg-orange text-white py-2 px-4 mr-2 hover:border-transparent rounded">
+      <button className="bg-orange text-white py-2 px-4 mr-2 hover:border-transparent rounded">
         {text}
       </button>
     </a>
@@ -11,7 +11,7 @@ export const Orange = ({ text }) => {
 export const Yellow = ({ text }) => {
   return (
     <a href="#!">
-      <button class="bg-transparent text-yellow py-2 px-4 border border-yellow rounded ml-2">
+      <button className="bg-transparent text-yellow py-2 px-4 border border-yellow rounded ml-2">
         {text}
       </button>
     </a>
