cc22774473b0a38076242a9f522ffe622c164415
Raj Patel <rajpatel158g@gmail.com>
2023-03-05 22:42:27 +0530

    Inserted WebComponents data

---
+++ src/data/ComponentsInfo.js
@@ -0,0 +1,180 @@
+/**
+ *  * Websites components data
+ */
+
+import alert from "../assets/componentsAssets/alert.png";
+import avtar from "../assets/componentsAssets/avtar.png";
+import badge from "../assets/componentsAssets/badge.png";
+import breadcumb from "../assets/componentsAssets/breadcumb.png";
+import button from "../assets/componentsAssets/button.png";
+import buttongroup from "../assets/componentsAssets/buttongroup.png";
+import card from "../assets/componentsAssets/card.png";
+import form from "../assets/componentsAssets/form.png";
+import footer from "../assets/componentsAssets/footer.png";
+import list from "../assets/componentsAssets/list.png";
+
+export const WebComponents = [
+  {
+    key: 0,
+    name: "Alerts",
+    icon: alert,
+    url: "#!",
+  },
+  {
+    key: 1,
+    name: "Avatar",
+    icon: avtar,
+    url: "#!",
+  },
+  {
+    key: 2,
+    name: "Badges",
+    icon: badge,
+    url: "#!",
+  },
+  {
+    key: 3,
+    name: "Breadcrumbs",
+    icon: breadcumb,
+    url: "#!",
+  },
+  {
+    key: 4,
+    name: "Button",
+    icon: button,
+    url: "#!",
+  },
+  {
+    key: 5,
+    name: "Button group",
+    icon: buttongroup,
+    url: "#!",
+  },
+  {
+    key: 6,
+    name: "Cards",
+    icon: card,
+    url: "#!",
+  },
+  {
+    key: 7,
+    name: "Forms",
+    icon: form,
+    url: "#!",
+  },
+  {
+    key: 8,
+    name: "Footer",
+    icon: footer,
+    url: "#!",
+  },
+
+  {
+    key: 9,
+    name: "List group",
+    icon: list,
+    url: "#!",
+  },
+  {
+    key: 10,
+    name: "Modal",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 11,
+    name: "Navbar",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 12,
+    name: "Pagination",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 13,
+    name: "Rating",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 14,
+    name: "Table",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 15,
+    name: "Input Field",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 16,
+    name: "File Input",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 17,
+    name: "Search bar",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 18,
+    name: "Select",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 19,
+    name: "Textarea",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 20,
+    name: "Radio",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 21,
+    name: "Checkbox",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 22,
+    name: "Hero",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 23,
+    name: "Heading",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 24,
+    name: "Paragraph",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 25,
+    name: "Split screen",
+    icon: "",
+    url: "#!",
+  },
+  {
+    key: 26,
+    name: "Image",
+    icon: "",
+    url: "#!",
+  },
+];
