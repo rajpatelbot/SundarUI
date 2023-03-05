e4126bd84e7b2b551e358af496a9fe15878e16c8
Raj Patel <rajpatel158g@gmail.com>
2023-03-05 22:45:41 +0530

    Created reusable card component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,19 @@
+import { TbExternalLink } from "react-icons/tb";
+
+const Card = ({ id, icon, text, href }) => {
+  return (
+    <a key={id} href={href}>
+      <div className="p-5 rounded-md shadow-md bg-white">
+        <div className="w-28 mb-5 m-auto">
+          <img className="w-full" src={icon} alt={text} />
+        </div>
+        <div className="mt-5 bg-light rounded-lg flex items-center justify-center">
+          <h3 className="text-center p-2">{text}</h3>
+          <TbExternalLink className="ml-2" />
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Card;
