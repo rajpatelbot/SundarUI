455380e604609be3a5347ff7a5c7b251203e95dd
Raj Patel <rajpatel158g@gmail.com>
2023-03-09 21:43:27 +0530

    remove unued line of code

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DataProvider>
       <App />
-      <div>dsv</div>
     </DataProvider>
   </React.StrictMode>,
 )
