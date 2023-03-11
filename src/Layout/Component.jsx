c543750a207247d1a4e2c059cd5a0c5934e72cb4
Raj Patel <rajpatel158g@gmail.com>
2023-03-12 00:36:50 +0530

    used reusable component properly

--- src/Layout/Component.jsx
+++ src/Layout/Component.jsx
@@ -1,10 +1,12 @@
 import Layoutheader from "../Components/Layoutheader";
 import Componentlayout from "../Components/Componentlayout";
+import { WebComponents } from "../data/ComponentsInfo";
 
 const Component = () => {
+  const webSubset = WebComponents.slice(0, 8);
 
   return (
-    <div className="padding-px py-24">
+    <div className="padding-px py-24 bg-lightRed">
       <div className="maxWidth m-auto">
         <Layoutheader
           title={"Components"}
@@ -12,8 +14,14 @@ const Component = () => {
             "Discover a vast assortment of more than 20 open-source UI components and interactive elements constructed using Tailwind CSS's utility classes."
           }
         />
-        <Componentlayout />
-        <Componentlayout />
+        <Componentlayout
+          webSubset={webSubset}
+          heading={"Tailwind CSS components"}
+        />
+        <Componentlayout
+          webSubset={webSubset}
+          heading={"Mobile components"}
+        />
       </div>
     </div>
   );
