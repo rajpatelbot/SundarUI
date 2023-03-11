68ea8e5038b0f47af17bd225d530cd05ae2897cf
Raj Patel <rajpatel158g@gmail.com>
2023-03-12 00:37:56 +0530

    add faqs

---
+++ src/Layout/Faqs.jsx
@@ -0,0 +1,43 @@
+import Layoutheader from "../Components/Layoutheader";
+import { Faqsdata } from "../data/Faqsdata";
+
+const Faqs = () => {
+  return (
+    <div className="padding-px py-24">
+      <div className="space-y-4 maxWidth m-auto">
+        <Layoutheader
+          title={"FAQs"}
+          desc={"Answers to Your Most Commonly Asked Questions."}
+        />
+        {Faqsdata.map((data) => (
+          <details
+            key={data.key}
+            className="group border-l-4 border-violet bg-white p-6"
+          >
+            <summary className="flex items-center justify-between cursor-pointer">
+              <h3 className="font-medium">{data.que}</h3>
+
+              <span className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="flex-shrink-0 w-5 h-5 transition duration-300 group-open:-rotate-45"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+            </summary>
+            <p className="mt-4 leading-relaxed">{data.ans}</p>
+          </details>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
---
+++ src/data/Faqsdata.js
@@ -0,0 +1,32 @@
+export const Faqsdata = [
+    {
+        key: 0,
+        que: "What is SundarUI?",
+        ans: "Sundar UI is a library of prebuilt user interface (UI) components that can be easily integrated into your web projects. The library provides a variety of components that are designed to be visually appealing and functional, including buttons, forms, modals, and more.",
+    },
+    {
+        key: 1,
+        que: "Is it paid or completely free and Open-Source?",
+        ans: "SundarUI doesn't charge any money for any of the components. It is completely free and Open-Source.",
+    },
+    {
+        key: 2,
+        que: "Benifits of SundarUI",
+        ans: "Sundar UI provides a variety of components like Buttons, Forms, Modals, Alerts, Icons etc.",
+    },
+    {
+        key: 3,
+        que: "How can I do my valuable contribution on SundarUI",
+        ans: "Newbe contributors are always welcome to contribute on SundarUI. If you want to do your contributions on it, you can do it by the help of reading contribution guidelines.",
+    },
+    {
+        key: 4,
+        que: "Is SundarUI have begginer friendly issues?",
+        ans: "Yes, It contains absolute begginer friendly issues.",
+    },
+    {
+        key: 5,
+        que: "What are the recommended skills to contribute on SundarUI?",
+        ans: "React.js, Css3, Tailwind CSS, XML & XSL. But if you don't know these skills, so still you can contribute on it's Readme.md files.",
+    },
+]
