ce64c4e78d1827a9575900fd7b36340f4d59a629
Raj Patel <rajpatel158g@gmail.com>
2023-03-12 00:35:39 +0530

    Refector the components and make it reusable

--- src/Components/Componentlayout.jsx
+++ src/Components/Componentlayout.jsx
@@ -1,22 +1,17 @@
 import Card from "./Card";
 import { Orange } from "./Button";
-import { WebComponents } from "../data/ComponentsInfo";
 
-const Componentlayout = () => {
-  const webSubset = WebComponents.slice(0, 8);
-
+const Componentlayout = ({ webSubset, heading }) => {
   return (
     <div className="mt-20">
-      <h2 className="mb-10 text-center font-medium text-black">
-        Tailwind CSS components
-      </h2>
-      <div className="grid grid-cols-4 gap-6">
+      <h2 className="mb-10 text-center font-medium text-black">{heading}</h2>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4">
         {webSubset.map((data) => (
           <Card
-            id={data.key}
+            key={data.key}
             icon={data.icon}
             text={data.name}
-            href={data.href}
+            url={data.url}
           />
         ))}
       </div>
