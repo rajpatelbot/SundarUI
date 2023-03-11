1af2959320070019044b75385a8a7a6daca59315
Raj Patel <rajpatel158g@gmail.com>
2023-03-12 00:37:27 +0530

    Add testonomials

---
+++ src/Components/Testimonialcard.jsx
@@ -0,0 +1,22 @@
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { RiDoubleQuotesR } from "react-icons/ri";
+
+const Testimonialcard = ({ key, name, msg, url, photo }) => {
+  return (
+    <div key={key} className="p-10 rounded-md shadow-md bg-white">
+      <div className="w-28 mb-5 m-auto">
+        <img className="w-full rounded-full" src={photo} alt={name} />
+      </div>
+      <a className="mt-2" href={url}>
+        <h3 className="text-center p-2">{name}</h3>
+      </a>
+      <p className="mt-2">
+        <RiDoubleQuotesL className="inline text-4xl mr-2 text-violet" />
+        {msg}
+        <RiDoubleQuotesR className="inline text-4xl ml-2 text-violet" />
+      </p>
+    </div>
+  );
+};
+
+export default Testimonialcard;
---
+++ src/data/Testinomialsdata.js
@@ -0,0 +1,34 @@
+export const Testinomialsdata = [
+  {
+    key: 0,
+    name: "Raj",
+    photo:
+      "https://pbs.twimg.com/profile_images/1593919524977672193/Rcn-D7uT_400x400.jpg",
+    msg: "Sundar UI's prebuilt components have saved me countless hours of development time. The components are well-designed, easily customizable, and have excellent documentation. I highly recommend Sundar UI to any developer looking for reliable components.",
+    url: "#!",
+  },
+  {
+    key: 1,
+    name: "Upendra Sahni",
+    photo:
+      "https://pbs.twimg.com/profile_images/1624829578773692416/YP2pciu8_400x400.jpg",
+    msg: "I was initially hesitant to use prebuilt components, but Sundar UI has exceeded my expectations. The components are not only functional, but also visually appealing, which is a major plus. Additionally, their customer support team is always quick to respond and provide helpful guidance.",
+    url: "#!",
+  },
+  {
+    key: 2,
+    name: "Dipesh Patel",
+    photo:
+      "https://pbs.twimg.com/profile_images/1460465930136285187/eu21bO5j_400x400.jpg",
+    msg: "I've been using Sundar UI's prebuilt components for several projects now, and I am continuously impressed with their quality and flexibility. The components are easily integrated with my projects and require minimal customization. Sundar UI's components have been a game changer for me.",
+    url: "#!",
+  },
+  {
+    key: 3,
+    name: "Aaditya Giri",
+    photo:
+      "https://pbs.twimg.com/profile_images/1550155124957335552/si6crI2W_400x400.jpg",
+    msg: "Sundar UI's prebuilt components are perfect for small teams or solo developers who need to create visually appealing projects quickly. The components are intuitive and easy to use, and the team at Sundar UI is always available to answer any questions or provide guidance. I highly recommend Sundar UI to any developer looking to streamline their development process.",
+    url: "#!",
+  },
+];
