014e4098fe20760687752afc013aa945e16bb69b
Raj Patel <rajpatel158g@gmail.com>
2023-03-16 00:13:32 +0530

    navigation setup with lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "./Container/Footer";
 import Navbar from "./Container/Navbar";
-import Homepage from "./pages/Homepage"
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Contribute = lazy(() => import("./pages/Contribute"));
+const About = lazy(() => import("./pages/About"));
+const Mobilecomponents = lazy(() => import("./pages/Mobilecomponents"));
+const Tailwindcomponents = lazy(() => import("./pages/Tailwindcomponents"));
+const Setupguide = lazy(() => import("./pages/Setupguide"));
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Homepage />
-      <Footer />
+      <Suspense fallback={"Loading..."}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/setupguides" element={<Setupguide />} />
+          <Route
+            exact
+            path="/tailwindcomponents"
+            element={<Tailwindcomponents />}
+          />
+          <Route
+            exact
+            path="/mobilecomponents"
+            element={<Mobilecomponents />}
+          />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contribute" element={<Contribute />} />
+        </Routes>
+        <Footer />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
