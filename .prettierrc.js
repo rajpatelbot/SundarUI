b65e9841ebf33bd7c4877312171afd14b6aadf0b
rajpatelbot <rajpatel158g@gmail.com>
2023-04-24 23:55:37 +0530

    chore: Update Prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "consistent",
+  arrowParens: "always",
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+};
