0bc57af92fa70beb1077a00aaecc1f2c299ec487
rajpatelbot <rajpatel158g@gmail.com>
2023-04-29 00:30:03 +0530

    feat(next.config): Add new configuration for eslint validation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ **/
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: { dirs: ['src'] },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
