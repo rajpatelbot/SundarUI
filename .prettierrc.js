a733af2d26a5621940c4d67abd10b3dc14cf7059
rajpatelbot <rajpatel158g@gmail.com>
2023-04-29 00:29:15 +0530

    feat(prettierrc): Add new rule for enforcing single quotes

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,9 +2,9 @@ module.exports = {
   printWidth: 120,
   semi: true,
   singleQuote: true,
-  quoteProps: "consistent",
-  arrowParens: "always",
-  trailingComma: "all",
+  quoteProps: 'consistent',
+  arrowParens: 'always',
+  trailingComma: 'all',
   tabWidth: 2,
   useTabs: false,
 };
