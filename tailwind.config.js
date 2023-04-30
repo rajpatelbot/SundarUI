7cacb8d3b2e0641e309cc03f50afa8d9be0ed587
rajpatelbot <rajpatel158g@gmail.com>
2023-04-30 23:15:11 +0530

    feat: Update Tailwind CSS config styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,9 @@ module.exports = {
         content: '1200px',
       },
       fontSize: {
-        '4xl': '2.5rem',
-        '2xl': '1.5rem',
-        'lg': '1.125rem',
+        '4xl': '2.25rem',
+        '2xl': '1.875rem',
+        'lg': '1rem',
         'sm': '0.875rem',
       },
     },
