7dbb540343d93005f13b29dc065ba69e644e245d
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-10-21 23:12:46 +0530

    fix: :memo: update the "edit this page on github" link
    changed the username and repo name in the edit link. Also changed the branch name from "main" to "master" as this project has it's main branch "master"

--- doc/.vitepress/config.ts
+++ doc/.vitepress/config.ts
@@ -67,7 +67,7 @@ export default defineConfig({
     ],
 
     editLink: {
-      pattern: "https://github.com/vuejs/vitepress/edit/main/docs/:path",
+      pattern: "https://github.com/rajpatelbot/SundarUI/edit/master/doc/:path",
       text: "Edit this page on GitHub",
     },
 
