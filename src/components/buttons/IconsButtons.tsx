0119e47ee39459d0285f2aeae9ef94c570394535
Ahsan khalid <ahsankhalid816@gmail.com>
2023-10-24 01:34:01 +0500

    Deployment done with all changes

--- src/components/buttons/IconsButtons.tsx
+++ src/components/buttons/IconsButtons.tsx
@@ -1,36 +1,88 @@
 import React from "react";
-import {
-  Linkedin,
-  Facebook,
-  RotateCcw,
-  CheckCircle,
-  Trash2,
-  SendHorizontal,
-} from "lucide-react";
+import { RotateCcw, CheckCircle, Trash2, SendHorizontal } from "lucide-react";
+import * as FaIcons from "react-icons/fa6";
+const {
+  FaLinkedin,
+  FaFacebook,
+  FaWhatsapp,
+  FaTelegram,
+  FaTwitter,
+  FaInstagram,
+  FaPinterest,
+  FaYoutube,
+  FaStackOverflow,
+  FaMedium,
+  FaReddit,
+} = FaIcons;
 
 export default function IconsButtons() {
   return (
     <div className="my-10">
-      {/* Social Media buttons */}
       <div className="flex items-center mb-8 flex-wrap gap-3">
-        {/* Linkedin button */}
-        <button className="bg-[#00A0DC] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#1f93bd] active:bg-[#017ca8] active:text-white flex items-center gap-1">
-          <Linkedin size={15} /> Linkedin
+        {/* Linkedin Button */}
+        <button className="bg-[#0587b7] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#1f93bd] active:bg-[#017ca8] active:text-white flex items-center gap-1">
+          <FaLinkedin className="w-6 h-6" />
+          LinkedIn
         </button>
 
-        {/* Facebook button */}
-        <button className="bg-[#385898] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#304d86] active:bg-[#26437c] active:text-white flex items-center gap-1">
-          <Facebook size={15} /> Facebook
+        {/* Facebook Button */}
+        <button className="bg-[#3e61a7] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#304d86] active-bg-[#26437c] active:text-white flex items-center gap-1">
+          <FaFacebook className="w-6 h-6" />
+          Facebook
         </button>
 
-        {/* WhatsApp button */}
-        <button className="bg-[#22C35E] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#15b650] active:bg-[#0a9a3f] active:text-white flex items-center gap-1">
+        {/* Whatsapp Button */}
+        <button className="bg-[#11c352] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#15b650] active-bg-[#0a9a3f] active:text-white flex items-center gap-1">
+          <FaWhatsapp className="w-6 h-6" />
           WhatsApp
         </button>
 
-        {/* Telegram button */}
-        <button className="bg-[#0088CC] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#147baf] active:bg-[#096b9c] active:text-white flex items-center gap-1">
+        {/* Telegram Button */}
+        <button className="bg-[#17a6ce] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#147baf] active-bg-[#096b9c] active:text-white flex items-center gap-1">
+          <FaTelegram className="w-6 h-6" />
           Telegram
+        </button>
+
+        {/* Twitter/X Button */}
+        <button className="bg-[#189df0] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#4c9de4] active-bg-[#1681ce] active:text-white flex items-center gap-1">
+          <FaTwitter className="w-6 h-6" />
+          Twitter
+        </button>
+
+        {/* Instagram Button */}
+        <button className="text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#d92562] active-bg-[#c31f56] active:text-white flex items-center gap-1 bg-gradient-to-r from-orange-500 via-red-500 to-blue-500">
+          <FaInstagram className="w-6 h-6" />
+          Instagram
+        </button>
+
+        {/* Pinterest Button */}
+        <button className="bg-[#f30909] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#d30020] active-bg-[#c1001d] active:text-white flex items-center gap-1">
+          <FaPinterest className="w-6 h-6" />
+          Pinterest
+        </button>
+
+        {/* Youtube Button */}
+        <button className="bg-[#ff1808] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#d90000] active-bg-[#c10000] active:text-white flex items-center gap-1">
+          <FaYoutube className="w-6 h-6" />
+          YouTube
+        </button>
+
+        {/* Stackoverflow Button */}
+        <button className="bg-gradient-to-r from-[#ae5410] via-[#e56e0d] to-[#e9700e] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#e6924d] active-bg-[#c96a1b] active:text-white flex items-center gap-1">
+          <FaStackOverflow className="w-6 h-6" />
+          Stack Overflow
+        </button>
+
+        {/* Medium Button */}
+        <button className="bg-[#040404] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#141514] active-bg-[#028553] active:text-white flex items-center gap-1">
+          <FaMedium className="w-6 h-6" />
+          Medium
+        </button>
+
+        {/* Reddit Button */}
+        <button className="bg-[#FF4500] text-white text-base font-semibold py-2 px-4 rounded-md duration-300 hover:bg-[#ff4400e7] active-bg-[#d13900] active:text-white flex items-center gap-1">
+          <FaReddit className="w-6 h-6" />
+          Reddit
         </button>
       </div>
 
